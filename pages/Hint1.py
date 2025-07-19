09c05755bb520a4e32cb1a50721225c022b2969c
RenVit318 <rens@renskievit.com>
2025-07-19 19:35:54 +0200

    Cheater Prevention

--- pages/Hint1.py
+++ pages/Hint1.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+if str(st.session_state.first_pass).lower() != st.secrets['first_pass']:
+    st.write('Wat doe je hier?? Ga terug naar de hoofdpagina!!')
+    st.stop()
+    
 st.title("Hint 1")
 answer = st.text_input(label="Antwoord op de puzzel", placeholder='...')
 if answer == st.secrets['hint1']:
